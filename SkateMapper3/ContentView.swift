c4f6eefea018351f2074248d6a795832641c64d0
Bryan Stober <bryanstober@gmail.com>
2021-12-01 20:05:43 -0600

    Initial Commit

---
+++ SkateMapper3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SkateMapper3
+//
+//  Created by bryan stober on 12/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SkateMapper3/SkateMapper3App.swift
@@ -0,0 +1,17 @@
+//
+//  SkateMapper3App.swift
+//  SkateMapper3
+//
+//  Created by bryan stober on 12/1/21.
+//
+
+import SwiftUI
+
+@main
+struct SkateMapper3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
